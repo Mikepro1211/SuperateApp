f92069e48f7b3a0d91863d889e5cd400e672b496
Jaredromero1 <jaredromero08@gmail.com>
2023-10-19 15:55:33 -0600

    agregando pantalla enfoque e impacto

--- Screens/Aliados.js
+++ Screens/Aliados.js
@@ -1,9 +1,28 @@
-import {Text, View} from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Aliados() {
-    return(
-        <View>
-            <Text>Aliados</Text>
+const Aliados = () => {
+
+  return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Pantalla Aliados</Text>
         </View>
-    )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+
+    },
+    titulo: {
+        color: "#00B6D8",
+        fontSize: 18,
+        textAlign: "center",
+        fontWeight: "bold",
+    },
+});
+
+export default Aliados;
