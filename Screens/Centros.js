625ca834a739f67ef67cc2d164adfd82da06b6cd
Mikepro <miguelalexander890@gmail.com>
2023-10-23 16:49:00 -0600

    Version two approved

--- Screens/Centros.js
+++ Screens/Centros.js
@@ -20,7 +20,8 @@ export default function Centros({navigation}){
             'https://superate.org.sv/wp-content/uploads/2018/11/3-2.jpg',
             'https://superate.org.sv/wp-content/uploads/2022/05/8.jpg',
             'https://superate.org.sv/wp-content/uploads/2022/07/hilasal_0010s_0004_Lineas.jpg',
-        ]
+        ],
+        correo:'superate@superate.com'
     },
         {
             id:2 ,
@@ -30,7 +31,7 @@ export default function Centros({navigation}){
             direccion: 'Calle Montecarmelo #800, Soyapango, San Salvador',
             telefono: '2277-0488',
             descripcion: 'Nuestro Centro ¡Supérate! ADOC se inauguró en el año 2008 gracias a la iniciativa de la Fundación ADOC y al gran aporte de Microsoft El Salvador con las licencias de “Microsoft Office”.',
-            logo: 'https://hilasal.clasessuperate.org/img/Logo.jpg',
+            logo: 'https://superate.org.sv/wp-content/uploads/2020/02/logo_adoc.gif',
             imagenes:[
                 'https://superate.org.sv/wp-content/uploads/2022/07/adoc_0009s_0000_Lineas.jpg',
                 'https://superate.org.sv/wp-content/uploads/2022/05/adoc_0009s_0004_Lineas-copy-6.jpg',
@@ -41,7 +42,8 @@ export default function Centros({navigation}){
                 'https://superate.org.sv/wp-content/uploads/2022/05/adoc_0009s_0003_Lineas-copy-5.jpg',
                 'https://superate.org.sv/wp-content/uploads/2022/07/adoc_0009s_0002_Lineas.jpg',
 
-            ]
+            ],
+            correo:'superate@superate.com'
         },
         {
             id:3 ,
@@ -51,7 +53,7 @@ export default function Centros({navigation}){
             direccion: ' Villa Guadalupe, Calle E San Miguelito, Panamá República de Panamá',
             telefono: ' (507) 396-3076',
             descripcion: 'Nuestro Centro ¡Supérate! Fundación Alberto Motta se abrió en el año 2010 gracias al esfuerzo y gran aporte de la Fundación Alberto Motta en la Ciudad de Panamá. Este fue el primer Centro ¡Supérate! que traspasó las fronteras salvadoreñas.',
-            logo: 'https://www.superate.org.sv/wp-content/uploads/2019/10/centro-alberto-motta.jpg',
+            logo: 'https://superate.org.sv/wp-content/uploads/2018/11/logo_motta.jpg',
             imagenes: [
                 'https://superate.org.sv/wp-content/uploads/2018/11/3-3.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/4-1-1.jpg',
@@ -61,7 +63,8 @@ export default function Centros({navigation}){
                 'https://superate.org.sv/wp-content/uploads/2018/11/8-2.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/9.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/2-2.jpg',
-            ]
+            ],
+            correo:'superate@superate.com'
         },
         {
             id:4 ,
@@ -71,7 +74,7 @@ export default function Centros({navigation}){
             direccion: '4ta calle poniente, entre 10a y 8a avenida sur, número 5-4, Santa Tecla, La Libertad',
             telefono: ' 2229-7239',
             descripcion: 'Nuestro Centro ¡Supérate! Fundación Poma se inauguró en el año 2011 gracias a la iniciativa de la Fundación Poma y al gran aporte de Microsoft El Salvador con las licencias de “Microsoft Office”.',
-            logo: 'https://www.superate.org.sv/wp-content/uploads/2019/10/centro-alberto-motta.jpg',
+            logo: 'https://superate.org.sv/wp-content/uploads/2018/11/logo_poma.jpg',
             imagenes:[
                 'https://superate.org.sv/wp-content/uploads/2022/07/slider_template_0005s_0000_Lineas.jpg',
                 'https://superate.org.sv/wp-content/uploads/2022/07/slider_template_0005s_0001_Lineas.jpg',
@@ -82,7 +85,8 @@ export default function Centros({navigation}){
                 'https://superate.org.sv/wp-content/uploads/2022/07/slider_template_0005s_0004_Lineas.jpg',
                 'https://superate.org.sv/wp-content/uploads/2022/07/slider_template_0005s_0005_Lineas.jpg',
 
-            ]
+            ],
+            correo:'superate@superate.com'
         },
         {
             id:5 ,
@@ -92,7 +96,7 @@ export default function Centros({navigation}){
             direccion: 'Calle Circunvalación, Polígono D #7, Plan de la Laguna, Antiguo Cuscatlán, La Libertad.',
             telefono: ' 2212-7500 ext 7516',
             descripcion: 'Nuestro Centro ¡Supérate! Merlet se inauguró en el año 2011 gracias a la Fundación Frech, al convenio firmado entre la Fundación Sagrera Palomo y USAID en el 2010 y la valiosa donación de licencias de Microsoft Office realizada por Microsoft El Salvador.',
-            logo: 'https://www.superate.org.sv/wp-content/uploads/2019/10/centro-alberto-motta.jpg',
+            logo: 'https://superate.org.sv/wp-content/uploads/2018/11/logo_merlet.jpg',
             imagenes: [
                 'https://superate.org.sv/wp-content/uploads/2022/07/merlet_0006s_0000_Lineas.jpg',
                 'https://superate.org.sv/wp-content/uploads/2022/07/merlet_0006s_0001_Lineas.jpg',
@@ -103,7 +107,8 @@ export default function Centros({navigation}){
                 'https://superate.org.sv/wp-content/uploads/2022/07/merlet_0006s_0003_Lineas.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/m3.jpg',
 
-            ]
+            ],
+            correo:'superate@superate.com'
 
 
         },
@@ -115,7 +120,7 @@ export default function Centros({navigation}){
             direccion: 'Av. Ricardo J Alfaro, Edificio Plaza Edison, Piso 5, Panamá',
             telefono: ' (507) 321-1595',
             descripcion: 'Nuestro Centro ¡Supérate! Fundación JUPÁ se inauguró en el año 2013, gracias a la iniciativa de la Fundación Judío-Panameña (JUPÁ) y la Embajada de los EE. UU. en Panamá, y la donación de licencias de Microsoft Office por Microsoft Panamá.',
-            logo: 'https://www.superate.org.sv/wp-content/uploads/2019/10/centro-alberto-motta.jpg',
+            logo: 'https://superate.org.sv/wp-content/uploads/2018/11/logo_jupa.jpg',
             imagenes: [
                 'https://superate.org.sv/wp-content/uploads/2018/11/1-2.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/2-5.jpg',
@@ -125,7 +130,8 @@ export default function Centros({navigation}){
                 'https://superate.org.sv/wp-content/uploads/2018/11/6-5.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/7-4.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/8-3.jpg',
-            ]
+            ],
+            correo:'superate@superate.com'
         },
         {
             id:7 ,
@@ -135,7 +141,7 @@ export default function Centros({navigation}){
             direccion: 'Intersección Carretera a San Miguel y Calle a Tonacatepeque después del Paso a desnivel de Unicentro Soyapango. Estadio “Plaza España”.',
             telefono: '2292-8464 ',
             descripcion: 'Nuestro Centro ¡Supérate! Fundación Raíces inicia operaciones en el año 2013 gracias al trabajo de la Fundación Raíces, al convenio firmado entre la Fundación Sagrera Palomo y USAID en el 2010 y la valiosa donación de licencias de Microsoft Office realizada por Microsoft El Salvador.',
-            logo: 'https://www.superate.org.sv/wp-content/uploads/2019/10/centro-alberto-motta.jpg',
+            logo: 'https://superate.org.sv/wp-content/uploads/2018/11/logo_raices.jpg',
             imagenes: [
                 'https://superate.org.sv/wp-content/uploads/2018/11/1-3.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/2-6.jpg',
@@ -145,7 +151,8 @@ export default function Centros({navigation}){
                 'https://superate.org.sv/wp-content/uploads/2018/11/5-6.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/7-5.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/8-4.jpg',
-            ]
+            ],
+            correo:'superate@superate.com'
         },
         {
             id:8 ,
@@ -155,7 +162,7 @@ export default function Centros({navigation}){
             direccion:'9ª Calle Oriente Nº 408 Barrio Concepción, San Miguel. (Ex Oficinas de AMNET)',
             telefono: '2606-0976',
             descripcion: 'Grupo Q se une a la familia ¡Supérate! en el año 2018 como patrocinador del Centro ubicado en San Miguel, El Salvador, gracias al apoyo de USAID y la valiosa donación de licencias de Microsoft Office realizada por Microsoft El Salvador.',
-            logo: 'https://www.superate.org.sv/wp-content/uploads/2019/10/centro-alberto-motta.jpg',
+            logo: 'https://superate.org.sv/wp-content/uploads/2018/11/LOGO-SUPERATE-GRUPO-Q.png',
             imagenes:[
                 'https://superate.org.sv/wp-content/uploads/2018/11/1-4.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/3-7.jpg',
@@ -163,7 +170,8 @@ export default function Centros({navigation}){
                 'https://superate.org.sv/wp-content/uploads/2022/07/adoc_0002s_0002_Lineas.jpg',
                 'https://superate.org.sv/wp-content/uploads/2022/07/adoc_0002s_0001_Lineas.jpg',
                 'https://superate.org.sv/wp-content/uploads/2022/07/adoc_0002s_0003_Lineas.jpg',
-            ]
+            ],
+            correo:'superate@superate.com'
 
         },
         {
@@ -174,7 +182,7 @@ export default function Centros({navigation}){
             direccion: ' Ciudad del Niño, Chorrera, Panamá',
             telefono: '(507) 258-0085',
             descripcion: 'El Centro ¡Supérate! Fundación Alberto Motta - La Chorrera inicia operaciones en el año 2018 gracias al esfuerzo y trabajo de la Fundación Alberto Motta en Panamá. Este se convierte en el tercer Centro ¡Supérate! en abrir sus puertas en Panamá.',
-            logo: 'https://www.superate.org.sv/wp-content/uploads/2019/10/centro-alberto-motta.jpg',
+            logo: 'https://superate.org.sv/wp-content/uploads/2018/11/logo_motta.jpg',
             imagenes:[
                 'https://superate.org.sv/wp-content/uploads/2018/11/5-7.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/6-7.jpg',
@@ -183,7 +191,8 @@ export default function Centros({navigation}){
                 'https://superate.org.sv/wp-content/uploads/2018/11/2-8.jpg',
                 'https://superate.org.sv/wp-content/uploads/2018/11/1-5.jpg',
 
-            ]
+            ],
+            correo:'superate@superate.com'
 
 
         }, {
@@ -194,7 +203,7 @@ export default function Centros({navigation}){
             direccion: 'Avenida Costanera, urbanización Versalles, calle Barcelona, edificio ¡Supérate!',
             telefono: '(507) 6670-7606',
             descripcion: 'El Centro ¡Supérate! Fundación Provivienda inicia operaciones en el 2019 gracias al esfuerzo y trabajo de la Fundación Provivienda. Éste se convierte en el cuarto Centro en Panamá.',
-            logo: 'https://www.superate.org.sv/wp-content/uploads/2019/10/centro-alberto-motta.jpg',
+            logo: 'https://superate.org.sv/wp-content/uploads/2019/04/LOGO-SUPERATE-F-Provivienda-01.png',
             imagenes:[
                 'https://superate.org.sv/wp-content/uploads/2022/05/1.jpg',
                 'https://superate.org.sv/wp-content/uploads/2019/04/pro-2.jpg',
@@ -202,7 +211,8 @@ export default function Centros({navigation}){
                 'https://superate.org.sv/wp-content/uploads/2019/04/1.jpg',
                 'https://superate.org.sv/wp-content/uploads/2019/04/3.jpg',
                 'https://superate.org.sv/wp-content/uploads/2019/04/2.jpg',
-            ]
+            ],
+            correo:'superate@superate.com'
 
         },
         {
@@ -213,11 +223,12 @@ export default function Centros({navigation}){
             direccion: 'Avenida Sur, Urbanización Miramar, Casa 1A, Santiago Veraguas.',
             telefono: '6553-3253',
             descripcion: 'El Centro ¡Supérate! Fundación Sus Buenos Vecinos entrará en operaciones en el 2023 gracias al esfuerzo y trabajo de la Fundación Sus Buenos Vecinos.',
-            logo: 'https://www.superate.org.sv/wp-content/uploads/2019/10/centro-alberto-motta.jpg',
+            logo: 'https://superate.org.sv/wp-content/uploads/2022/12/logo_pa_fsbv.png',
             imagenes: [
                 'https://superate.org.sv/wp-content/uploads/2022/12/1-fsbv.jpg',
                 'https://superate.org.sv/wp-content/uploads/2022/12/2-fsbv.jpg',
-            ]
+            ],
+            correo:'superate@superate.com'
 
         }
     ]
--- Screens/InformacionCentro.js
+++ Screens/InformacionCentro.js
@@ -1,31 +1,136 @@
-import { View, Text ,StyleSheet, Image, ScrollView} from 'react-native';
-import {Carousel} from "react-native-ui-lib";
-
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Carousel } from "react-native-ui-lib";
+import { Card } from 'react-native-paper';
+import { FontAwesome } from '@expo/vector-icons';
 export default function InformacionCentro({navigation , route}) {
     const {centro} = route.params
     return(
-        <View style={styles.container}>
+        <ScrollView>
+            <View style={styles.container}>
 
-            <Carousel onChangePage={()=> console.log("change")} showCounter={true} autoplay={true}  loop={true} >
-                {centro.imagenes.map((imagen, index) => (
-                    <Image key={index} style={{ width: 400, height: 250 }} source={{ uri: imagen }}  />))}
-            </Carousel>
-            <View style={styles.containerStudent}>
-            <Text>{centro.estudiantes}</Text>
+                <Carousel onChangePage={()=> console.log("change")} showCounter={true} autoplay={true}  loop={true} >
+                    {centro.imagenes.map((imagen, index) => (
+                        <Image key={index} style={{ width: 400, height: 250 }} source={{ uri: imagen }}  />))}
+                </Carousel>
+                <Text style={styles.nombreCentro}>{centro.nombre}</Text>
+                <View style={styles.containerStudentGraduados}>
+                    <View  style={styles.graduadoContainer}>
+                        <Text style={styles.textGraduados}> {centro.graduados}</Text>
+                    </View>
+                    <Text> Graduados</Text>
+                </View>
+
+                <View style={styles.containerStudentGraduados}>
+                    <View style={styles.estudianteContainer}>
+                        <Text  style={styles.textGraduados}> {centro.estudiantes}</Text>
+                    </View>
+                    <Text> Estudiantes</Text>
+                </View>
+
+                <View style={styles.containerDos}>
+                    <Image style={styles.imagen} source={{uri: centro.logo}}/>
+                    <Text style={styles.descripcion}>{centro.descripcion}</Text>
+                </View>
+                < View>
+                    <Card style={styles.card}>
+                        <Card.Title   title="Información de contacto" />
+                        <Card.Content>
+                            <View style={styles.contactoContainer}>
+                                <FontAwesome name="envelope" size={24} color="black" />
+                                <Text style={styles.contactoText}>{centro.correo}</Text>
+                            </View>
+                            <View style={styles.contactoContainer}>
+                                <FontAwesome name="phone" size={24} color="black" />
+                                <Text style={styles.contactoText}>{centro.telefono}</Text>
+                            </View>
+                            <View style={styles.contactoContainer}>
+                                <FontAwesome name="map-marker" size={24} color="black" />
+                                <Text style={styles.contactoText}>{centro.direccion}</Text>
+                            </View>
+                            <View style={styles.redesContainer}>
+                                <FontAwesome name="facebook" size={24} color="black" />
+                                <FontAwesome name="twitter" size={24} color="black" />
+                                <FontAwesome name="instagram" size={24} color="black" />
+                            </View>
+                        </Card.Content>
+                    </Card>
+                </View>
             </View>
-        </View>
+        </ScrollView>
+
     )
 }
 
 
+
 const styles = StyleSheet.create({
+
+    card: {
+        margin: 16,
+        padding: 15,
+        backgroundColor: '#F2F2F2'
+    },
+    contactoContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 8,
+        padding:8,
+    },
+    contactoText: {
+        fontSize: 16,
+        marginLeft: 8,
+    },
+    redesContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        marginTop: 24,
+    },
+    descripcion:{
+        textAlign:'justify',
+        alignItems:'center',
+    },
+
+    containerDos:{
+        flex:  2,
+        backgroundColor:' red',
+        margin: 16,
+        padding: 16,
+    },
+    nombreCentro:{
+        color: '#00B6D8',
+        fontSize: 25,
+        margin: 25,
+        textAlign: 'center',
+    },
+    estudianteContainer:{
+        backgroundColor: '#8DC63F',
+        padding: 15,
+        margin: 'auto',
+    },
+    graduadoContainer:{
+        backgroundColor: '#00B6D8',
+        padding: 15,
+        margin: 'auto',
+    },
+    textGraduados:{
+        fontSize: 20,
+        color: 'white',
+    },
+    containerStudentGraduados:{
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        padding: 10,
+
+    },
 
     container :{
         flex: 1,
+        backgroundColor: '#fff',
 
     },
     Text:{
-      fontSize:100,
+        fontSize:100,
     },
     imagen:{
         width:'100%',
