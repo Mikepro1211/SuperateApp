43a9b019e13c88756a91966682bad9dbb602ccbc
Mikepro <miguelalexander890@gmail.com>
2023-10-24 23:54:18 -0600

    Actualizacion Formulario

--- Screens/Contacto.js
+++ Screens/Contacto.js
@@ -1,10 +1,55 @@
-import {Text ,View} from "react-native";
+import {Text ,View ,StyleSheet, ScrollView , KeyboardAvoidingView} from "react-native";
+import {TextField} from "react-native-ui-lib";
+import {TextInput} from "react-native-paper";
+import { Platform } from "react-native";
 
 
 export default  function Contacto(){
     return(
-        <View>
-            <Text>Contacto</Text>
-        </View>
+        <ScrollView style={styles.container}>
+        
+            <View style={styles.ContactConainer}>
+                <Text style={styles.contactText}>Contacta las oficinas institucionales</Text>
+             </View>
+            <KeyboardAvoidingView style={{padding:16 , backgroundColor:'#ff3'}}behavior={Platform.OS=='ios'?'padding':'height'}>
+              
+                <TextField style={styles.input}
+                    placeholder="Nombre"
+                    floatingPlaceholder
+                    floatOnFocus
+                    containerStyle={{marginBottom: 20}}/>
+                   
+
+                
+            </KeyboardAvoidingView>
+            
+              
+        </ScrollView>
     )
 }
+
+
+
+const styles = StyleSheet.create({
+    input:{
+        width:"100%",
+        padding:10,
+        color:'#000',
+    },
+    container:{
+        flex:1,
+        padding:16,
+        backgroundColor:"#ffff"
+    },
+    ContactConainer:{
+        flex:1,
+        flexDirection:"column",
+        
+       
+        backgroundColor:"#ffff",
+        width:"100%",
+        padding:16
+    },
+    
+    
+})
